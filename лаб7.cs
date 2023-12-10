928bc73fce67f4a358a2b239c3aabb062f4a7936
956784 <146649071+956784@users.noreply.github.com>
2023-12-10 21:10:00 +0300

    Add files via upload

---
+++ лаб7.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите строку");
+string S = Console.ReadLine();
+if (S.Length > N)
+    S = S.Substring(S.Length - N, N);
+else
+    S = new string('.', N - S.Length) + S;
+Console.WriteLine(S);
